5b213590d629fd602db5b504d982c9d982dabc5a
Sam <ksamaksh0601@gmail.com>
2025-09-25 17:00:17 -0400

    #206 reverse linked list

---
+++ LCmac/ordered/linkedlist/reverselist.py
@@ -0,0 +1,10 @@
+prev = None
+        cur = head
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+
+        return prev
+        
