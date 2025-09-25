2a68c649ea2e86a11e3926200d27067936f5e4a5
Sam <ksamaksh0601@gmail.com>
2025-09-25 17:09:43 -0400

    merge two sorted list #21

---
+++ LCmac/ordered/linkedlist/mergetwosortedlinkedlist.py
@@ -0,0 +1,15 @@
+res = ListNode()
+        head = res 
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                res.next = list1
+                list1 = list1.next
+            else:
+                res.next = list2
+                list2 = list2.next
+            res = res.next
+
+        res.next = list1 or list2
+
+        return head.next
