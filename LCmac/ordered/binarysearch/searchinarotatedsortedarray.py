7b2d45b18ac429d7444c5178e4b6ff2b3373c517
Sam <ksamaksh0601@gmail.com>
2025-09-25 16:41:53 -0400

    search in a rotated sorted array #33

---
+++ LCmac/ordered/binarysearch/searchinarotatedsortedarray.py
@@ -0,0 +1,22 @@
+l = 0
+r = len(nums)-1
+if nums[l] == target:
+        return l
+if nums[r] == target:
+        return r
+while l<=r:
+    m = (l+r)//2
+    if nums[m] == target:
+        return m
+    if nums[l] <= nums[m]:
+        if nums[l] <= target < nums[m]:
+            r = m - 1
+        else:
+            l = m + 1
+
+    else:
+        if nums[m] <= target < nums[r]:
+            l = m + 1
+        else:
+            r = m - 1
+return -1
